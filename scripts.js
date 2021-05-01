e9b0c5c97f089bb1163b190ad3f07a720fc7f5db
Roberto Sousa <robertosousa186@gmail.com>
2021-04-30 23:37:51 -0300

    creating javascript's script

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function hidemSobreMim() {
+  const container = document.getElementById("sobre_mim");
+
+  if (container.style.display === "none") {
+    container.style.display = "block";
+  } else {
+    container.style.display = "none";
+  }
+}
+
+function hidemMeusProjetos() {
+  const container = document.getElementById("meusprojetos");
+
+  if (container.style.display === "none") {
+    container.style.display = "block";
+  } else {
+    container.style.display = "none";
+  }
+}
+
+function hidemSkills() {
+  const container = document.getElementById("skills");
+
+  if (container.style.display === "none") {
+    container.style.display = "block";
+  } else {
+    container.style.display = "none";
+  }
+}
